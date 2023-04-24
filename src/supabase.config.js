91b32a85d19d4e64ed7c7b1eed8b7254f258a66e
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-24 01:50:39 -0500

    Remove log

--- src/supabase.config.js
+++ src/supabase.config.js
@@ -2,5 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_KISS_A_FISH_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_KISS_A_FISH_SUPABASE_API_KEY;
-console.log(supabaseUrl, supabaseKey);
 export const supabase = createClient(supabaseUrl, supabaseKey);
