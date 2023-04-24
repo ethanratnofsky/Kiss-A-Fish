0dee75e1712e1eb9bc4cfea787c51ecf736592c6
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-24 02:14:36 -0500

    Style create post form

--- src/routes/CreatePost.jsx
+++ src/routes/CreatePost.jsx
@@ -30,27 +30,39 @@ const CreatePost = () => {
     return (
         <div className="create-post-container">
             <form onSubmit={handleSubmit}>
-                <h1>Post your fish kiss!</h1>
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                    id="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <label htmlFor="content">Content</label>
-                <textarea
-                    id="content"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                />
-                <label htmlFor="image-url">Image URL</label>
-                <input
-                    type="text"
-                    id="image-url"
-                    value={imageUrl}
-                    onChange={(e) => setImageUrl(e.target.value)}
-                />
+                <h1 className="heading">Share a Fish Kiss</h1>
+                <div className="input-container">
+                    <label htmlFor="title">Title</label>
+                    <span className="input-description">Create a title for your post</span>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="eg. My first fish kiss!"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="content">Content</label>
+                    <span className="input-description">Write a description for your post</span>
+                    <textarea
+                        id="content"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="eg. I was so excited to kiss my first fish! I've been fishing for weeks and finally caught one!"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="image-url">Image URL</label>
+                    <span className="input-description">Add an image to your post</span>
+                    <input
+                        type="text"
+                        id="image-url"
+                        value={imageUrl}
+                        onChange={(e) => setImageUrl(e.target.value)}
+                        placeholder="eg. https://www.mywebsite.com/my-image.jpg"
+                    />
+                </div>
                 <button type="submit">Post</button>
             </form>
         </div>
