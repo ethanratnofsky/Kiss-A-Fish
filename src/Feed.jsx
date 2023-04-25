1671dd16c233b54dfcf1e8858b3b50bfaf07db53
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-04-24 20:39:03 -0500

    Add search and filter

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -3,9 +3,18 @@ import { Link } from "react-router-dom";
 import { supabase } from "./supabase.config";
 import "./Feed.css";
 
+const FILTERS = {
+    DATE_ASC: "DATE_ASC",
+    DATE_DESC: "DATE_DESC",
+    UPVOTES_ASC: "UPVOTES_ASC",
+    UPVOTES_DESC: "UPVOTES_DESC",
+};
+
 const Feed = () => {
     const [posts, setPosts] = useState([]);
-    const [sortedPosts, setSortedPosts] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filter, setFilter] = useState(FILTERS.DATE_DESC);
 
     const fetchPosts = async () => {
         const { data, error } = await supabase.from("Posts").select();
@@ -21,19 +30,78 @@ const Feed = () => {
         fetchPosts();
     }, []);
 
-    // Sort posts by newest first
+    // Fitler posts by search term and sort by filter
     useEffect(() => {
-        const sortedPosts = posts.sort((a, b) => {
-            return new Date(b.created_at) - new Date(a.created_at);
-        });
-        setSortedPosts(sortedPosts);
-    }, [posts]);
+        let filteredPosts = [...posts];
+
+        // Filter by search term
+        if (searchTerm) {
+            filteredPosts = filteredPosts.filter((post) =>
+                post.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        }
+
+        // Sort by filter
+        switch (filter) {
+            case FILTERS.DATE_ASC:
+                filteredPosts.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+                break;
+            case FILTERS.DATE_DESC:
+                filteredPosts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+                break;
+            case FILTERS.UPVOTES_ASC:
+                filteredPosts.sort((a, b) => a.upvotes - b.upvotes);
+                break;
+            case FILTERS.UPVOTES_DESC:
+                filteredPosts.sort((a, b) => b.upvotes - a.upvotes);
+                break;
+            default:
+                break;
+        }
+
+        setFilteredPosts(filteredPosts);
+    }, [posts, searchTerm, filter]);
+
+    const handleSearchTermChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleToggleDateSort = () => {
+        if (filter === FILTERS.DATE_DESC) setFilter(FILTERS.DATE_ASC);
+        else setFilter(FILTERS.DATE_DESC); // default sort
+    };
+
+    const handleToggleUpvotesSort = () => {
+        if (filter === FILTERS.UPVOTES_DESC) setFilter(FILTERS.UPVOTES_ASC);
+        else setFilter(FILTERS.UPVOTES_DESC); // default sort
+    };
 
     return (
         <div className="feed-container">
-            {sortedPosts.length > 0 ? (
+            <div className="filters-container">
+                <input
+                    className="search-bar"
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleSearchTermChange}
+                    placeholder="Search Kiss-A-Fish..."
+                />
+                <button onClick={handleToggleDateSort}>
+                    {filter === FILTERS.DATE_ASC ? "↑ " : filter === FILTERS.DATE_DESC ? "↓ " : ""}
+                    Date Posted
+                </button>
+                <button onClick={handleToggleUpvotesSort}>
+                    {filter === FILTERS.UPVOTES_ASC
+                        ? "↑ "
+                        : filter === FILTERS.UPVOTES_DESC
+                        ? "↓ "
+                        : ""}
+                    Upvotes
+                </button>
+            </div>
+            {filteredPosts.length > 0 ? (
                 <ul className="posts-list">
-                    {sortedPosts.map((post) => (
+                    {filteredPosts.map((post) => (
                         <Link to={"/" + post.id} key={post.id} className="post-list-item-container">
                             <li className="post-list-item">
                                 <div className="post-preview">
